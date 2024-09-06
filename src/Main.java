bc79d61ca6621154f6e205603d1ee7fe6bfbe9c8
GoldHouse777 <psoltysik01@gmail.com>
2024-09-06 10:11:50 +0200

    losowanie liczby rzeczywistej

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("test test test");
+        System.out.println("na lekcji programowanka");
+
+        double wylosowanaLiczba = Math.random()*100+1;
+        System.out.println(wylosowanaLiczba);
     }
 }
